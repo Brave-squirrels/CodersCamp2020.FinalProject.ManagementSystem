c2b46b38a68f663a3f5dd3eb643b0068befa0ff9
Michal <michal.smalira@gmail.com>
2021-02-19 14:26:36 +0100

    bugfix: typo in get endpoint

--- Server/src/projects/getSpecifiedProjectFromTeam.ts
+++ Server/src/projects/getSpecifiedProjectFromTeam.ts
@@ -3,7 +3,7 @@ import projectModel from '../../models/projects.model';
 import { StatusCodes } from 'http-status-codes';
 
 const getSpecifiedProjectFromTeam = async(req: Request, res: Response) => {
-    const project = await projectModel.find({ _id: req.params.proejctId });
+    const project = await projectModel.find({ _id: req.params.projectId });
     if(!project) return res.status(StatusCodes.NOT_FOUND).send('Project not found');
 
     return res.status(StatusCodes.OK).send(project);
