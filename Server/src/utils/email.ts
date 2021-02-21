0a8346799424399d52363913a982965895975bd2
Aleksander <aleksander.atamanczuk@gmail.com>
2021-02-21 22:22:39 +0100

    refactor: set new email client

--- Server/src/utils/email.ts
+++ Server/src/utils/email.ts
@@ -1,23 +1,27 @@
 import nodemailer from "nodemailer";
 import "dotenv/config";
 
-export default function sednEmail(email: string, url: string) {
+export default async function sednEmail(email: string, url: string) {
   const transporter = nodemailer.createTransport({
-    service: "hotmail",
+    host: "smtp.mail.yahoo.com",
+    port: 465,
+    service: "yahoo",
+    secure: false,
     auth: {
       user: process.env.EMAIL,
       pass: process.env.PASSWORD,
     },
+    debug: false,
   });
 
   const mailOptions = {
-    from: "management.system2021@gmail.com",
+    from: process.env.EMAIL,
     to: email,
     subject: "Confirm Email",
     html: `Please click this link to confirm your email: <a href="${url}">${url}</a>`,
   };
 
-  transporter.sendMail(mailOptions, function (err) {
+  transporter.sendMail(mailOptions, function (err, data) {
     if (err) console.log("Error Occurs: ", err);
     else console.log("Email sent!!!");
   });
