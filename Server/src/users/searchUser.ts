88a189c946c1eb0121f005fc306b003e4220c478
Olek <aleksander.atamanczuk@gmail.com>
2021-02-27 20:38:57 +0100

    refactor: search user

--- Server/src/users/searchUser.ts
+++ Server/src/users/searchUser.ts
@@ -1,27 +1,16 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-import mongoose from "mongoose";
 import _ from "lodash";
 import userModel from "../../models/user.model";
-import User from "../../interfaces/user.interface";
-
-type userType = (User & mongoose.Document<any>)[];
 
 const searchUsers = async (req: Request, res: Response) => {
-  const tagsArray = req.params.tags ? req.params.tags.split("_") : "";
-
+  const tags = req.params.tags ? req.params.tags.split("_") : "";
   const user = await userModel.find().sort("email").select("id name email");
-
-  const result = filterByValue(user, tagsArray);
+  const result = !tags
+    ? user
+    : user.filter((u) => tags.every((t) => u.email.toLowerCase().includes(t)));
 
   res.status(StatusCodes.OK).send(result);
 };
-
-const filterByValue = (users: userType, tags: string[] | "") =>
-  !tags
-    ? users
-    : users.filter((user) =>
-        tags.every((tag) => user.email.toLowerCase().includes(tag))
-      );
 
 export default searchUsers;
