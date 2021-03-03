c3bf030b70173a0c59e7d3ae09706081bd984b23
Cyprian Zamiara <veryyapeee@gmail.com>
2021-03-03 17:17:30 +0100

    fixbug: fixing comments and tasks get authorization

--- Server/middleware/authGetTasksComments.ts
+++ Server/middleware/authGetTasksComments.ts
@@ -3,13 +3,14 @@ import {Request, Response, NextFunction} from 'express';
 
 const authCommentsTasks = (req: Request,res: Response,next : NextFunction)=>{
     const project = res.locals.project;
+    const team = res.locals.team;
     let usr = false;
     project.members.forEach((e: any)=>{
         if(e.id==req.userInfo._id){
             usr = true;
         }
     })
-    if(!usr){
+    if(!usr && !team.moderatorsId.includes(req.userInfo._id) && team.ownerId !== req.userInfo._id){
         return res.status(StatusCodes.UNAUTHORIZED).send('Access denied');
     }
     next();
