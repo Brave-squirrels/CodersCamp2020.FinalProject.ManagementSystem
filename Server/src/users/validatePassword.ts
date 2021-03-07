5751f2a341bb7733f9d365aa2634237dccfaae12
Olek <aleksander.atamanczuk@gmail.com>
2021-03-07 19:29:53 +0100

    refactor: user test

--- Server/src/users/validatePassword.ts
+++ Server/src/users/validatePassword.ts
@@ -3,8 +3,8 @@ import User from "../../interfaces/user.interface";
 
 export default function validatePassword(user: User) {
   const schema = Joi.object({
-    password: Joi.string().min(8).max(255).required(),
-    confirmPassword: Joi.string().min(8).max(255).required(),
+    password: Joi.string().min(4).max(255).required(),
+    confirmPassword: Joi.string().min(4).max(255).required(),
   });
 
   return schema.validate(user);
