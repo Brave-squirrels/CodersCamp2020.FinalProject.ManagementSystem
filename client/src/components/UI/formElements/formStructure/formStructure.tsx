d2263bd2a5746627f42b137e2e0dea7221b10620
Cyprian Zamiara <veryyapeee@gmail.com>
2021-03-14 20:43:58 +0100

    feature: validation refactor

---
+++ client/src/components/UI/formElements/formStructure/formStructure.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import Input from "../input/input";
+
+interface Props {
+  state: any;
+  onChangeHandler: any;
+}
+
+const formStructure = (props: Props) => {
+  let key: keyof typeof props.state;
+  let elements = [];
+  for (key in props.state) {
+    elements.push({
+      id: key,
+      config: props.state[key],
+    });
+  }
+
+  const form = elements.map((input: any) => {
+    return (
+      <Input
+        key={input.id}
+        type={input.config.type}
+        placeholder={input.config.placeholder}
+        inputValue={input.config.val}
+        onChangeInput={(e: { target: HTMLInputElement }) =>
+          props.onChangeHandler(e, input.id)
+        }
+        label={input.config.label}
+        validity={input.config.valid}
+        touched={input.config.touched}
+      />
+    );
+  });
+
+  return <>{form}</>;
+};
+
+export default formStructure;
