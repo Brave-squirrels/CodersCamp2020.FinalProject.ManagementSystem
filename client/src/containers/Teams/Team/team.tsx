d1d2d8f56fd84249e80246345c5d2fb287b68042
Maciej Chojak <maciekch7991@gmail.com>
2021-03-25 14:38:16 +0100

    feature: add component CardWithTitle and applied it in single team view

--- client/src/containers/Teams/Team/team.tsx
+++ client/src/containers/Teams/Team/team.tsx
@@ -9,6 +9,7 @@ import ViewWithSidebar from "hoc/viewWithSidebar/viewWithSidebar";
 import SideBar from "components/UI/sideBar/sideBar";
 import styles from "./team.module.scss";
 import TeamSidebar from "./teamSideBar/teamSideBar";
+import { CardWithTitle } from "components/UI/CardWithTitle/CardWithTitle";
 
 import { fetchTeam } from "reduxState/teamDataSlice";
 
@@ -23,8 +24,12 @@ const Team = () => {
     dispatch(fetchTeam(teamId));
   }, [dispatch, teamId]); // eslint-disable-line react-hooks/exhaustive-deps
 
-  const ownerIndex = state.team.members.findIndex((member: any) => state.team.ownerId)
-  
+  const moderatorsList = state.team.moderatorsId.map((moderatorId: string) =>
+    state.team.members.map((member: any) =>
+      member.userId === state.team.ownerId ? member.userName : null
+    )
+  );
+
   return (
     <ViewWithSidebar>
       <SideBar>
@@ -37,35 +42,25 @@ const Team = () => {
           {/* Container for team's info */}
           <div className={styles.container}>
             <div className={styles.firstColumn}>
-              <div className={styles.owner}>
-                <p className={styles.tileTitle}>Owner</p>
-                <div className={styles.content}>{state.team.members[ownerIndex].userName}</div>
-              </div>
+              <CardWithTitle title={"Owner"}>
+                {state.team.members.map((member: any) =>
+                  member.userId === state.team.ownerId ? member.userName : null
+                )}
+              </CardWithTitle>
 
-              <div className={styles.description}>
-                <p className={styles.tileTitle}>Creation date</p>
-                <div className={styles.content}>{(state.team.startDate).match(/[0-9]{4}-[0-9]{2}-[0-9]{2}/)}</div>
-              </div>
+              <CardWithTitle title={"Creation date"}>
+                {state.team.startDate.match(/[0-9]{4}-[0-9]{2}-[0-9]{2}/)}
+              </CardWithTitle>
 
-              <div className={styles.description}>
-                <p className={styles.tileTitle}>Description</p>
-                <div className={styles.content}>{state.team.description}</div>
-              </div>
-
+              <CardWithTitle title={"Description"}>
+                {state.team.description}
+              </CardWithTitle>
             </div>
 
-            <div className={styles.members}>
-              <p className={styles.tileTitle}>Members</p>
-              <div className={styles.content}>
-                {state.team.members.map((member: any) => member.userName)}
-              </div>
-            </div>
-            <div className={styles.moderators}>
-              <p className={styles.tileTitle}>Moderators</p>
-              <div className={styles.content}>
-                {state.team.moderatorsId.map((id: any) => id)}
-              </div>
-            </div>
+            <CardWithTitle title={"Members"}>
+              {state.team.members.map((member: any) => member.userName)}
+            </CardWithTitle>
+            <CardWithTitle title={"Moderators"}>{moderatorsList}</CardWithTitle>
           </div>
         </div>
       </div>
