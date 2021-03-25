23d3cbc23677c978ad50d5bc44844d934fbd11c0
Maciej Chojak <maciekch7991@gmail.com>
2021-03-25 13:23:47 +0100

    feature: remove projects from single team view

--- client/src/containers/Teams/Team/team.tsx
+++ client/src/containers/Teams/Team/team.tsx
@@ -8,9 +8,6 @@ import * as types from "utils/types";
 import ViewWithSidebar from "hoc/viewWithSidebar/viewWithSidebar";
 import SideBar from "components/UI/sideBar/sideBar";
 import styles from "./team.module.scss";
-// import classes from "./user.module.scss";
-import NavigationItem from "components/UI/navigationItem/navigationItem";
-import Card from "components/UI/Card/card";
 import TeamSidebar from "./teamSideBar/teamSideBar";
 
 import { fetchTeam } from "reduxState/teamDataSlice";
@@ -26,6 +23,8 @@ const Team = () => {
     dispatch(fetchTeam(teamId));
   }, [dispatch, teamId]); // eslint-disable-line react-hooks/exhaustive-deps
 
+  const ownerIndex = state.team.members.findIndex((member: any) => state.team.ownerId)
+  
   return (
     <ViewWithSidebar>
       <SideBar>
@@ -38,23 +37,21 @@ const Team = () => {
           {/* Container for team's info */}
           <div className={styles.container}>
             <div className={styles.firstColumn}>
-              
+              <div className={styles.owner}>
+                <p className={styles.tileTitle}>Owner</p>
+                <div className={styles.content}>{state.team.members[ownerIndex].userName}</div>
+              </div>
+
+              <div className={styles.description}>
+                <p className={styles.tileTitle}>Creation date</p>
+                <div className={styles.content}>{(state.team.startDate).match(/[0-9]{4}-[0-9]{2}-[0-9]{2}/)}</div>
+              </div>
+
               <div className={styles.description}>
                 <p className={styles.tileTitle}>Description</p>
                 <div className={styles.content}>{state.team.description}</div>
               </div>
 
-              <div className={styles.owner}>
-                <p className={styles.tileTitle}>Owner</p>
-                <div className={styles.content}>{state.team.ownerId}</div>
-              </div>
-
-              <div className={styles.moderators}>
-                <p className={styles.tileTitle}>Moderators</p>
-                <div className={styles.content}>
-                  {state.team.moderatorsId.map((id: any) => id)}
-                </div>
-              </div>
             </div>
 
             <div className={styles.members}>
@@ -63,20 +60,10 @@ const Team = () => {
                 {state.team.members.map((member: any) => member.userName)}
               </div>
             </div>
-
-            <div className={styles.projects}>
-              <p className={styles.tileTitle}>Projects</p>
-              <div className={styles.projectsContainer}>
-                {state.team.projects.map(({ name, id }: any) => (
-                  <NavigationItem
-                    path={`/teams/${teamId}/projects/${id}`}
-                    key={id}
-                  >
-                    <Card key={id}>
-                      <h3>{name}</h3>
-                    </Card>
-                  </NavigationItem>
-                ))}
+            <div className={styles.moderators}>
+              <p className={styles.tileTitle}>Moderators</p>
+              <div className={styles.content}>
+                {state.team.moderatorsId.map((id: any) => id)}
               </div>
             </div>
           </div>
