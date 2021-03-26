3c55ac1b6b1ee4bee9757acf55f7ffc4c7b32a76
Veryape <veryyapeee@gmail.com>
2021-03-26 16:07:22 +0100

    feature: handle teamInvite fetch

--- client/src/components/teamInviteContent/teamInviteContent.tsx
+++ client/src/components/teamInviteContent/teamInviteContent.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-/* import { useDispatch } from "react-redux"; */
+import { useDispatch, useSelector } from "react-redux";
 
 import Button from "../UI/formElements/button/button";
 import ButtonDanger from "../UI/formElements/buttonDanger/buttonDanger";
+import SpinnerCard from "components/UI/SpinnerCard/spinner";
+import ErrorHandler from "components/errorHandler/errorHandler";
 
-/* import { RootState } from "reduxState/store"; */
+import { RootState } from "reduxState/store";
+import { acceptInvite, declineInvite } from "reduxState/teamInvites";
 
 import styles from "./teamInviteContent.module.scss";
 
@@ -14,22 +17,39 @@ interface Props {
 }
 
 const TeamInviteContent = (props: Props) => {
-  /* const dispatch = useDispatch(); */
+  const dispatch = useDispatch();
+  const state = useSelector((state: RootState) => state.handleTeamInvite);
 
   const acceptHandler = () => {
-    /* console.log(props.teamId); */
+    dispatch(acceptInvite(props.teamId));
   };
 
   const declineHandler = () => {
-    /* console.log(props.teamId); */
+    dispatch(declineInvite(props.teamId));
   };
 
   return (
-    <div className={styles.wrapper}>
-      <span className={styles.inviteTitle}>{props.children}</span>
-      <Button clicked={acceptHandler}>Accept</Button>
-      <ButtonDanger clicked={declineHandler}>Decline</ButtonDanger>
-    </div>
+    <>
+      {state.success ? (
+        <>{state.information}</>
+      ) : (
+        <>
+          {state.error ? (
+            <ErrorHandler>{state.error.response.data}</ErrorHandler>
+          ) : state.loading ? (
+            <SpinnerCard />
+          ) : (
+            <div className={styles.wrapper}>
+              <>
+                <span className={styles.inviteTitle}>{props.children}</span>
+                <Button clicked={acceptHandler}>Accept</Button>
+                <ButtonDanger clicked={declineHandler}>Decline</ButtonDanger>
+              </>
+            </div>
+          )}
+        </>
+      )}
+    </>
   );
 };
 
