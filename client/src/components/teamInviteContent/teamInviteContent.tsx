7c0d8a5525f95f4901cb4eea72c14317a6493f48
Veryape <veryyapeee@gmail.com>
2021-03-26 16:14:47 +0100

    refactor: demo team invite done - no fetch stages handle

--- client/src/components/teamInviteContent/teamInviteContent.tsx
+++ client/src/components/teamInviteContent/teamInviteContent.tsx
@@ -1,13 +1,7 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
 
 import Button from "../UI/formElements/button/button";
 import ButtonDanger from "../UI/formElements/buttonDanger/buttonDanger";
-import SpinnerCard from "components/UI/SpinnerCard/spinner";
-import ErrorHandler from "components/errorHandler/errorHandler";
-
-import { RootState } from "reduxState/store";
-import { acceptInvite, declineInvite } from "reduxState/teamInvites";
 
 import styles from "./teamInviteContent.module.scss";
 
@@ -17,39 +11,12 @@ interface Props {
 }
 
 const TeamInviteContent = (props: Props) => {
-  const dispatch = useDispatch();
-  const state = useSelector((state: RootState) => state.handleTeamInvite);
-
-  const acceptHandler = () => {
-    dispatch(acceptInvite(props.teamId));
-  };
-
-  const declineHandler = () => {
-    dispatch(declineInvite(props.teamId));
-  };
-
   return (
-    <>
-      {state.success ? (
-        <>{state.information}</>
-      ) : (
-        <>
-          {state.error ? (
-            <ErrorHandler>{state.error.response.data}</ErrorHandler>
-          ) : state.loading ? (
-            <SpinnerCard />
-          ) : (
-            <div className={styles.wrapper}>
-              <>
-                <span className={styles.inviteTitle}>{props.children}</span>
-                <Button clicked={acceptHandler}>Accept</Button>
-                <ButtonDanger clicked={declineHandler}>Decline</ButtonDanger>
-              </>
-            </div>
-          )}
-        </>
-      )}
-    </>
+    <div className={styles.wrapper}>
+      <span className={styles.inviteTitle}>{props.children}</span>
+      <Button>Accept</Button>
+      <ButtonDanger>Decline</ButtonDanger>
+    </div>
   );
 };
 
