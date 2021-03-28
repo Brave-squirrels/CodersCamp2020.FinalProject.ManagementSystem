7c926087f6bf3daa0135ca205cfae0259ac7784a
Veryape <veryyapeee@gmail.com>
2021-03-28 14:18:46 +0200

    feature: display teams cards

--- client/src/containers/Teams/Teams/teams.tsx
+++ client/src/containers/Teams/Teams/teams.tsx
@@ -1,11 +1,17 @@
+import { useSelector } from "react-redux";
+import { RootState } from "reduxState/store";
+
 import TeamSidebar from "../Team/teamSideBar/teamSideBar";
 import ViewWithSidebar from "hoc/viewWithSidebar/viewWithSidebar";
 import styles from "./teams.module.scss";
+import NavigationItem from "components/UI/navigationItem/navigationItem";
 
 import CardContainer from "components/UI/CardContainer/cardContainer";
 import Card from "components/UI/Card/card";
 
 const Teams = () => {
+  const user = useSelector((state: RootState) => state.login.userInformation);
+
   return (
     <ViewWithSidebar>
       <TeamSidebar />
@@ -13,18 +19,17 @@ const Teams = () => {
         <div className={styles.wrapper}>
           <CardContainer title="Your Teams">
             <div className={styles.innerWrapper}>
-              <Card />
-              <Card />
-              <Card />
-              <Card />
-              <Card />
-              <Card />
-              <Card />
-              <Card />
-              <Card />
-              <Card />
-              <Card />
-              <Card />
+              {user.teams && user.teams.length ? (
+                user.teams.map((el: any) => (
+                  <NavigationItem path={`/teams/${el.id}`} key={el.id}>
+                    <Card key={el.id}>
+                      <h3 className={styles.cardHeader}>{el.name}</h3>
+                    </Card>
+                  </NavigationItem>
+                ))
+              ) : (
+                <div>You have not joined any teams yet...</div>
+              )}
             </div>
           </CardContainer>
         </div>
