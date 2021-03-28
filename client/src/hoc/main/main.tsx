30ef7c0715ba61bc605acad4919fd99765571a0c
Veryape <veryyapeee@gmail.com>
2021-03-29 00:30:35 +0200

    feature: add particles

--- client/src/hoc/main/main.tsx
+++ client/src/hoc/main/main.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import ParticlesCon from "./particles";
 
 import styles from "./main.module.scss";
 
 const main = (props: any) => {
-  return <main className={styles.mainCon}>{props.children}</main>;
+  return (
+    <main className={styles.mainCon}>
+      {props.children}
+      <ParticlesCon />
+    </main>
+  );
 };
 
 export default main;
