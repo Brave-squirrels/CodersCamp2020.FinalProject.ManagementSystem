25169933d88141783d4d6f729c7f0cda339f1bc4
Veryape <veryyapeee@gmail.com>
2021-03-29 12:17:47 +0200

    feature: project side bar done

--- client/src/containers/Projects/Project/projectSidebar/projectSidebar.tsx
+++ client/src/containers/Projects/Project/projectSidebar/projectSidebar.tsx
@@ -1,38 +1,40 @@
 import React from "react";
-import { /*  useHistory, */ useParams, NavLink } from "react-router-dom";
+import { useHistory, useParams, NavLink } from "react-router-dom";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import * as types from "../../../../utils/types";
-/* import styles from "./projectSidebar.module.scss";
 
 import PrimaryActiveItem from "components/UI/sideBar/sidebarItems/primaryActiveItem/primaryActiveItem";
 import PrimaryInactiveItem from "components/UI/sideBar/sidebarItems/primaryInactiveItem/primaryInactiveItem";
 import SecondaryItem from "components/UI/sideBar/sidebarItems/secondaryItem/secondaryItem";
 import PrimaryList from "components/UI/sideBar/sidebarItems/primaryList/primaryList";
 import SecondaryList from "components/UI/sideBar/sidebarItems/secondaryList/secondaryList";
-import LiItem from "components/UI/sideBar/sidebarItems/liItem/liItem"; */
+import LiItem from "components/UI/sideBar/sidebarItems/liItem/liItem";
 import SideBar from "components/UI/sideBar/sideBar";
 import SpinnerLight from "components/UI/spinnerLight/spinner";
 
 import { RootState } from "reduxState/store";
 import { fetchProject } from "reduxState/projectDataSlice";
+import { fetchTeam } from "reduxState/teamDataSlice";
 
 const ProjectSidebar = () => {
   const dispatch = useDispatch();
-  /*  const history = useHistory(); */
+  const history = useHistory();
 
   const { teamId, projectId } = useParams<types.TParams>();
 
   const state = useSelector((state: RootState) => state.singleProjectData);
   const user = useSelector((state: RootState) => state.login.userInformation);
-  /*   const userTeam = useSelector((state: RootState) => state.singleTeamData); */
 
   useEffect(() => {
-    console.log(state);
-    console.log(user);
     dispatch(fetchProject(teamId, projectId));
+    dispatch(fetchTeam(teamId));
   }, [dispatch, projectId]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const changeProject = (e: any) => {
+    history.push(`/teams/${teamId}/projects/${e.target.id}`);
+  };
 
   return (
     <SideBar
@@ -42,7 +44,49 @@ const ProjectSidebar = () => {
         </NavLink>
       }
     >
-      <SpinnerLight />
+      <PrimaryList>
+        {user.projects ? (
+          user.projects.map((prj: any) => {
+            if (prj.teamId === teamId) {
+              if (prj.id === projectId) {
+                return (
+                  <LiItem teamId={prj.id} key={prj.id}>
+                    <PrimaryActiveItem name={prj.name} />
+                    <SecondaryList>
+                      <NavLink
+                        to={`/teams/${teamId}/projects/${prj.id}/notes`}
+                        exact
+                        key={`${prj.id}notes`}
+                      >
+                        <SecondaryItem id={prj.id} name={"Notes"} />
+                      </NavLink>
+                      <NavLink
+                        to={`/teams/${teamId}/projects/${prj.id}/tasks`}
+                        exact
+                        key={`${prj.id}tasks`}
+                      >
+                        <SecondaryItem id={prj.id} name={"Tasks"} />
+                      </NavLink>
+                    </SecondaryList>
+                  </LiItem>
+                );
+              } else {
+                return (
+                  <PrimaryInactiveItem
+                    key={prj.id}
+                    id={prj.id}
+                    name={prj.name}
+                    clickHandler={changeProject}
+                  />
+                );
+              }
+            }
+            return null;
+          })
+        ) : (
+          <SpinnerLight />
+        )}
+      </PrimaryList>
     </SideBar>
   );
 };
