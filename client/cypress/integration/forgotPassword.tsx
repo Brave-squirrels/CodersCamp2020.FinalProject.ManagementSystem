6423e056993050aa9b5b706881e0214da10d3deb
Cyprian Zamiara <veryyapeee@gmail.com>
2021-04-13 13:25:12 +0200

    feature: add tests for notes, create new user, create team, forgotPassword

---
+++ client/cypress/integration/forgotPassword.tsx
@@ -0,0 +1,20 @@
+describe("Test createTeam", () => {
+  beforeEach(() => {
+    // @ts-ignore
+    cy.viewport(1200, 850);
+    cy.visit("/forgotpassword");
+    cy.wait(500);
+  });
+
+  it("Should send reset password mail", () => {
+    const mail = "test@gmail.com";
+    cy.get("[class*=input_inputContainer] input")
+      .clear({ force: true })
+      .type(mail);
+    cy.get("[class*=formStructure_form] > button").click({ force: true });
+  });
+  it("Should return to main page onClick", () => {
+    cy.get("[class*=forgotPassword_goBackBtn] > button").click();
+    cy.location("pathname").should("eq", "/");
+  });
+});
